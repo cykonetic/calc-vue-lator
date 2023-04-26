6ea9e0459532925e52231a0bdfe01f95713783b2
nicholaibush-rr <116030006+nicholaibush-rr@users.noreply.github.com>
2023-04-25 22:49:35 -0400

    project updates, fixes

--- src/components/CalculatorApp.vue
+++ src/components/CalculatorApp.vue
@@ -1,14 +1,33 @@
 <script setup lang="ts">
-import { reactive, computed } from "vue";
+import { reactive } from "vue";
 import CalculatorButton from "./CalculatorButton.vue";
 import CalculatorDisplay from "./CalculatorDisplay.vue";
 
+const buttonClassClear = "bg-yellow-600 text-slate-900 col-span-2";
+const buttonClassDigit = "bg-slate-700 text-zinc-200";
+const buttonClassOperator = "bg-orange-600 text-zinc-200";
+const buttonClassEquals = "bg-slate-400 text-slate-900 col-span-2";
+
+const displayDecimal = ".";
+const displayError = "Error";
+const displayDefault = "0";
+
+const operatorAdd = "+";
+const operatorClear = "C";
+const operatorDivide = "÷";
+const operatorEqual = "=";
+const operatorMultiply = "×";
+const operatorNegate = "±";
+const operatorSubtract = "-";
+const operatorDefault = operatorAdd;
+
 interface Calculator {
-  register: number;
-  operator: string;
-  display: string;
-  waiting: boolean;
-  executed: boolean;
+  register: number; // value of left-side (stored)
+  operator: string | null; // operation
+  display: string; // value of right-side
+  waiting: boolean; // user input expected (defaults will complete operations)
+  executed: boolean; // last operation complete
+  error: boolean; // last calculation Infinity or NaN
 }
 
 interface HandleInput {
@@ -23,63 +42,73 @@ interface Button {
 
 const state: Calculator = reactive({
   register: 0,
-  operator: "+",
-  display: "0",
+  operator: null,
+  display: displayDefault,
   waiting: true,
   executed: true,
+  error: false,
 });
 
 const buttons: Button[] = [
+  { symbol: operatorClear, onPressed: clear, class: buttonClassClear },
+  { symbol: operatorEqual, onPressed: equals, class: buttonClassEquals },
+
+  { symbol: "7", onPressed: setDigit, class: buttonClassDigit },
+  { symbol: "8", onPressed: setDigit, class: buttonClassDigit },
+  { symbol: "9", onPressed: setDigit, class: buttonClassDigit },
   {
-    symbol: "C",
-    onPressed: clear,
-    class: "bg-yellow-600 text-slate-900 col-span-2",
-  },
-  {
-    symbol: "=",
-    onPressed: equals,
-    class: "bg-slate-400 text-slate-900 col-span-2",
-  },
-  { symbol: "7", onPressed: setDigit, class: "bg-slate-700 text-zinc-200" },
-  { symbol: "8", onPressed: setDigit, class: "bg-slate-700 text-zinc-200" },
-  { symbol: "9", onPressed: setDigit, class: "bg-slate-700 text-zinc-200" },
-  {
-    symbol: "÷",
+    symbol: operatorDivide,
     onPressed: setOperator,
-    class: "bg-orange-600 text-zinc-200",
+    class: buttonClassOperator,
   },
-  { symbol: "4", onPressed: setDigit, class: "bg-slate-700 text-zinc-200" },
-  { symbol: "5", onPressed: setDigit, class: "bg-slate-700 text-zinc-200" },
-  { symbol: "6", onPressed: setDigit, class: "bg-slate-700 text-zinc-200" },
+
+  { symbol: "4", onPressed: setDigit, class: buttonClassDigit },
+  { symbol: "5", onPressed: setDigit, class: buttonClassDigit },
+  { symbol: "6", onPressed: setDigit, class: buttonClassDigit },
   {
-    symbol: "×",
+    symbol: operatorMultiply,
     onPressed: setOperator,
-    class: "bg-orange-600 text-zinc-200",
+    class: buttonClassOperator,
   },
-  { symbol: "1", onPressed: setDigit, class: "bg-slate-700 text-zinc-200" },
-  { symbol: "2", onPressed: setDigit, class: "bg-slate-700 text-zinc-200" },
-  { symbol: "3", onPressed: setDigit, class: "bg-slate-700 text-zinc-200" },
+
+  { symbol: "1", onPressed: setDigit, class: buttonClassDigit },
+  { symbol: "2", onPressed: setDigit, class: buttonClassDigit },
+  { symbol: "3", onPressed: setDigit, class: buttonClassDigit },
   {
-    symbol: "-",
+    symbol: operatorSubtract,
     onPressed: setOperator,
-    class: "bg-orange-600 text-zinc-200",
+    class: buttonClassOperator,
   },
-  { symbol: "±", onPressed: negate, class: "bg-slate-700 text-zinc-200" },
-  { symbol: "0", onPressed: setDigit, class: "bg-slate-700 text-zinc-200" },
-  { symbol: ".", onPressed: setDecimal, class: "bg-slate-700 text-zinc-200" },
-  {
-    symbol: "+",
-    onPressed: setOperator,
-    class: "bg-orange-600 text-zinc-200",
-  },
+
+  { symbol: operatorNegate, onPressed: negate, class: buttonClassDigit },
+  { symbol: "0", onPressed: setDigit, class: buttonClassDigit },
+  { symbol: displayDecimal, onPressed: setDecimal, class: buttonClassDigit },
+  { symbol: operatorAdd, onPressed: setOperator, class: buttonClassOperator },
 ];
+
+function clear(): void {
+  state.register = 0;
+  state.operator = null;
+  state.display = displayDefault;
+  state.waiting = true;
+  state.executed = true;
+  state.error = false;
+}
+
+function clearError(): void {
+  if (!state.error) {
+    return;
+  }
+  state.display = displayDefault;
+  state.error = false;
+}
 
 function calculate(
   operator: string,
   leftOpperand: number,
   rightOpperand: number
-): number {
-  let result;
+): string {
+  let result: number;
   switch (operator) {
     case "×":
       result = leftOpperand * rightOpperand;
@@ -93,65 +122,66 @@ function calculate(
     default: // case "+":
       result = leftOpperand + rightOpperand;
   }
+  state.error = Number.isNaN(result) || Infinity === result;
 
-  return parseFloat(result.toFixed(7));
-}
-
-function clear(): void {
-  state.register = 0;
-  state.operator = "+";
-  state.display = "0";
-  state.waiting = true;
-  state.executed = true;
+  return state.error ? displayError : parseFloat(result.toFixed(7)).toString();
 }
 
 function negate(): void {
+  !state.error || clearError();
   const input = parseFloat(state.display);
-  const result = calculate("×", -1.0, input);
-  state.display = parseFloat(result.toFixed(7)).toString();
-}
-
-function setDecimal(): void {
-  if (state.waiting) {
-    state.display = "0.";
-    state.waiting = false;
-    return;
-  }
-  if (!/\./.test(state.display)) {
-    state.display += ".";
-  }
-}
-
-function setDigit(key: string): void {
-  if (state.waiting) {
-    state.display = key;
-    state.waiting = false;
-    return;
-  }
-  state.display = "0" === state.display ? key : state.display + key;
+  state.display = calculate(operatorMultiply, -1.0, input);
 }
 
 function equals(): void {
+  !state.error || clearError();
   let input = parseFloat(state.display);
-
-  if (!state.executed) {
+  if (!(state.executed || state.waiting)) {
     const swap = state.register;
     state.register = input;
     input = state.waiting ? 0 : swap;
   }
-
-  const result = calculate(state.operator, state.register, input);
-  state.display = result.toString();
+  state.display = calculate(
+    state.operator ?? operatorDefault,
+    input,
+    state.register
+  );
   state.executed = state.waiting = true;
 }
 
-function setOperator(key: string): void {
-  if (!state.waiting && !state.executed) {
-    const input = parseFloat(state.display);
-    const result = calculate(state.operator, state.register, input);
-    state.display = result.toString();
+function setDecimal(): void {
+  if (/\./.test(state.display)) {
+    return;
   }
+  if (!state.waiting) {
+    state.display += displayDecimal;
+    return;
+  }
+  !state.error || clearError();
+  state.display = displayDefault + displayDecimal;
+  state.waiting = false;
+}
 
+function setDigit(key: string): void {
+  if (!state.waiting) {
+    state.display =
+      displayDefault === state.display ? key : state.display + key;
+    return;
+  }
+  !state.error || clearError();
+  state.display = key;
+  state.waiting = false;
+}
+
+function setOperator(key: string): void {
+  !state.error || clearError();
+  if (!(state.executed || state.waiting)) {
+    state.display = calculate(
+      state.operator ?? operatorDefault,
+      state.register,
+      parseFloat(state.display)
+    );
+  }
   state.register = parseFloat(state.display);
   state.operator = key;
   state.executed = false;
